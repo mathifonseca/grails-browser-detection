484db3d69ab8e3fae7d3eb0001fbdfe35a3f583f
Mathi Fonseca <mathifonseca@gmail.com>
2016-03-03 10:29:41 -0300

    fixed bug in isRobot method

--- grails-app/services/org/geeks/browserdetection/UserAgentIdentService.groovy
+++ grails-app/services/org/geeks/browserdetection/UserAgentIdentService.groovy
@@ -229,7 +229,7 @@ class UserAgentIdentService extends WebTierService {
 	}
 
 	boolean isRobot() {
-		userAgent.browserType == BrowserType.ROBOT
+		userAgent.browser.browserType == BrowserType.ROBOT
 	}
 
 	/**
